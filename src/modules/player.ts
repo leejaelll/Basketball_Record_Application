6d3e68dad2b61b7c3a74b5561e7473631714e45e
leejaelll <jayaom91076@gmail.com>
2023-05-02 15:23:08 +0900

    feat: action creator 함수 작성 & 리듀서 함수 작성 완료

--- src/modules/player.ts
+++ src/modules/player.ts
@@ -7,16 +7,11 @@ export const INCREASE_ASSIST = 'INCREASE_ASSIST';
 export const DECREASE_ASSIST = 'DECREASE_ASSIST';
 export const INCREASE_STEAL = 'INCREASE_STEAL';
 export const DECREASE_STEAL = 'DECREASE_STEAL';
+export const INCREASE_FOUL = 'INCREASE_FOUL';
+export const DECREASE_FOUL = 'DECREASE_FOUL';
 export const ADD_TO_TODAY_PLAYERS = 'ADD_TO_TODAY_PLAYERS';
 
 /* ------------------------ 액션 함수 만들기(Action Creator) ----------------------- */
-// export const increaseScore = (playerId) => {
-//   return {
-//     type: INCREASE_SCORE,
-//     payload: playerId,
-//   };
-// };
-
 export const addToTodayPlayers = (player, teamName) => {
   return {
     type: ADD_TO_TODAY_PLAYERS,
@@ -26,15 +21,92 @@ export const addToTodayPlayers = (player, teamName) => {
       rebound: 0,
       assist: 0,
       steal: 0,
+      foul: 0,
       teamName,
     },
   };
 };
-// export const decreaseScore = () => {
-//   return {
-//     type: DECREASE_SCORE,
-//   };
-// };
+
+export const increaseScore = (playerId) => {
+  return {
+    type: INCREASE_SCORE,
+    payload: {
+      playerId,
+    },
+  };
+};
+export const decreaseScore = (playerId) => {
+  return {
+    type: DECREASE_SCORE,
+    payload: {
+      playerId,
+    },
+  };
+};
+export const increaseRebound = (playerId) => {
+  return {
+    type: INCREASE_REBOUND,
+    payload: {
+      playerId,
+    },
+  };
+};
+export const decreaseRebound = (playerId) => {
+  return {
+    type: DECREASE_REBOUND,
+    payload: {
+      playerId,
+    },
+  };
+};
+export const increaseAssist = (playerId) => {
+  return {
+    type: INCREASE_ASSIST,
+    payload: {
+      playerId,
+    },
+  };
+};
+export const decreaseAssist = (playerId) => {
+  return {
+    type: DECREASE_ASSIST,
+    payload: {
+      playerId,
+    },
+  };
+};
+export const increaseSteal = (playerId) => {
+  return {
+    type: INCREASE_STEAL,
+    payload: {
+      playerId,
+    },
+  };
+};
+export const decreaseSteal = (playerId) => {
+  return {
+    type: DECREASE_STEAL,
+    payload: {
+      playerId,
+    },
+  };
+};
+export const increaseFoul = (playerId) => {
+  return {
+    type: INCREASE_FOUL,
+    payload: {
+      playerId,
+    },
+  };
+};
+export const decreaseFoul = (playerId) => {
+  return {
+    type: DECREASE_FOUL,
+    payload: {
+      playerId,
+    },
+  };
+};
 
 // 초기상태선언
 export const initialState = {
@@ -64,20 +136,120 @@ export default function counterReducer(state = initialState, action) {
         ...state,
         todayPlayers: [...state.todayPlayers, action.payload],
       };
+    case INCREASE_SCORE:
+      const idxIncreaseScore = state.todayPlayers.findIndex(
+        (el) => el.id === action.payload.playerId,
+      );
 
-    // // payload로 idx를 받아야함
-    // const idx = state.players.findIndex(
-    //   (el) => el.id === action.payload.playerId,
-    // );
-    // return {
-    //   ...state,
-    //   players: [
-    //     ...state.players.slice(0, idx),
-    //     action.payload,
-    //     ...state.players.slice(idx + 1),
-    //   ],
-    // };
+      return {
+        ...state,
+        todayPlayers: state.todayPlayers.map((el, idx) =>
+          idx === idxIncreaseScore ? { ...el, score: (el.score += 1) } : el,
+        ),
+      };
+    case DECREASE_SCORE:
+      const idxDecreaseScore = state.todayPlayers.findIndex(
+        (el) => el.id === action.payload.playerId,
+      );
 
+      return {
+        ...state,
+        todayPlayers: state.todayPlayers.map((el, idx) =>
+          idx === idxDecreaseScore ? { ...el, score: (el.score -= 1) } : el,
+        ),
+      };
+    case INCREASE_REBOUND:
+      const idxIncreaseRebound = state.todayPlayers.findIndex(
+        (el) => el.id === action.payload.playerId,
+      );
+
+      return {
+        ...state,
+        todayPlayers: state.todayPlayers.map((el, idx) =>
+          idx === idxIncreaseRebound
+            ? { ...el, rebound: (el.rebound += 1) }
+            : el,
+        ),
+      };
+    case DECREASE_REBOUND:
+      const idxDecreaseRebound = state.todayPlayers.findIndex(
+        (el) => el.id === action.payload.playerId,
+      );
+
+      return {
+        ...state,
+        todayPlayers: state.todayPlayers.map((el, idx) =>
+          idx === idxDecreaseRebound
+            ? { ...el, rebound: (el.rebound -= 1) }
+            : el,
+        ),
+      };
+    case INCREASE_ASSIST:
+      const idxIncreaseAssist = state.todayPlayers.findIndex(
+        (el) => el.id === action.payload.playerId,
+      );
+
+      return {
+        ...state,
+        todayPlayers: state.todayPlayers.map((el, idx) =>
+          idx === idxIncreaseAssist ? { ...el, assist: (el.assist += 1) } : el,
+        ),
+      };
+    case DECREASE_ASSIST:
+      const idxDecreaseAssist = state.todayPlayers.findIndex(
+        (el) => el.id === action.payload.playerId,
+      );
+
+      return {
+        ...state,
+        todayPlayers: state.todayPlayers.map((el, idx) =>
+          idx === idxDecreaseAssist ? { ...el, assist: (el.assist -= 1) } : el,
+        ),
+      };
+    case INCREASE_STEAL:
+      const idxIncreaseSteal = state.todayPlayers.findIndex(
+        (el) => el.id === action.payload.playerId,
+      );
+
+      return {
+        ...state,
+        todayPlayers: state.todayPlayers.map((el, idx) =>
+          idx === idxIncreaseSteal ? { ...el, steal: (el.steal += 1) } : el,
+        ),
+      };
+    case DECREASE_STEAL:
+      const idxDecreaseSteal = state.todayPlayers.findIndex(
+        (el) => el.id === action.payload.playerId,
+      );
+
+      return {
+        ...state,
+        todayPlayers: state.todayPlayers.map((el, idx) =>
+          idx === idxDecreaseSteal ? { ...el, steal: (el.steal -= 1) } : el,
+        ),
+      };
+    case INCREASE_FOUL:
+      const idxIncreaseFoul = state.todayPlayers.findIndex(
+        (el) => el.id === action.payload.playerId,
+      );
+
+      return {
+        ...state,
+        todayPlayers: state.todayPlayers.map((el, idx) =>
+          idx === idxIncreaseFoul ? { ...el, foul: (el.foul += 1) } : el,
+        ),
+      };
+    case DECREASE_FOUL:
+      const idxDecreaseFoul = state.todayPlayers.findIndex(
+        (el) => el.id === action.payload.playerId,
+      );
+
+      return {
+        ...state,
+        todayPlayers: state.todayPlayers.map((el, idx) =>
+          idx === idxDecreaseFoul ? { ...el, foul: (el.foul -= 1) } : el,
+        ),
+      };
     default:
       return state;
   }
