992427f3c01ef2294c57f41f8ced971dfa9e983c
leejaelll <jayaom91076@gmail.com>
2023-05-03 07:26:42 +0900

    배포 테스트4

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ function App() {
       <Header />
 
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/Basketball_Record_Application/" element={<Home />} />
         <Route path="/setting" element={<SelectPlayerPage />} />
         <Route path="/boxscore" element={<BoxScorePage />} />
         <Route path="/result" element={<GameResultPage />} />
