061c9658cc31d39a4f078ad0a5ca3b340b044868
leejaelll <jayaom91076@gmail.com>
2023-05-02 15:10:12 +0900

    feat: FoulCircle 컴포넌트 완성
    - 클릭했을 때 파울 갯수가 증가하도록 구현했습니다.
    - 5개가 다 진한 원으로 만들어진 이후에 한 번 더 클릭하면 파울 갯수가 0으로 초기화되도록 만들었습니다.

---
+++ src/components/FoulCircle.tsx
@@ -0,0 +1,21 @@
+export default function FoulCircle({ foulCount, setFoul }) {
+  // props로 받은 갯수에 따라 원 렌더링하기
+  // 5개의 원 중에서 foulCount 숫자만큼 원을 칠하고 나머지는 회색원으로 만든다.
+  const handleClick = () => {
+    setFoul((foul) => (foul === 5 ? 0 : foul + 1));
+  };
+
+  return (
+    <ul>
+      {new Array(5).fill(0).map((_, idx) => (
+        <li
+          key={idx}
+          className={`w-6 h-6 rounded-full ${
+            idx + 1 <= foulCount ? 'bg-slate-900' : 'bg-slate-600'
+          }`}
+          onClick={handleClick}
+        ></li>
+      ))}
+    </ul>
+  );
+}
