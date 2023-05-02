381e503467c5df0ba404172e8b68d30311f634e0
leejaelll <jayaom91076@gmail.com>
2023-05-03 06:28:31 +0900

    conf: vite.confing.ts 내 base 설정 변경

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Basketball_Record_Application/',
   plugins: [react()],
-})
+});
