b209fa0bb0463529d4e5cb6cf540df02662c8425
leejaelll <jayaom91076@gmail.com>
2023-05-02 20:23:55 +0900

    Header 이미지 클릭 시 초기화

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,23 @@
+import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
+
+import { initTodayPlayers } from '../modules/player';
+
 import logo from '../assets/logo.png';
 
 export default function Header() {
+  const dispatch = useDispatch();
+
   return (
     <header>
       <div>
+        {/* TODO: Home버튼을 눌렀을 때 todayPlayers가 초기화되어야 함 */}
         <Link to="/">
-          <img src={logo} className="w-20" />
+          <img
+            src={logo}
+            className="w-20"
+            onClick={() => dispatch(initTodayPlayers())}
+          />
         </Link>
       </div>
     </header>
