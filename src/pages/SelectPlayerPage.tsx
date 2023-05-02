02497e2fd514a1c3a70bbd6a99f87cb5b7a0d685
leejaelll <jayaom91076@gmail.com>
2023-05-03 07:09:57 +0900

    배포 테스트3

--- src/pages/SelectPlayerPage.tsx
+++ src/pages/SelectPlayerPage.tsx
@@ -15,7 +15,7 @@ export default function SelectPlayerPage() {
           type="button"
           className="text-4xl btn-45 after:content-[''] after:absolute after:bg-black after:rounded-lg after:h-12 after:left-0 after:w-full after:translate-x-2 after:translate-y-2 after:transform after:transition-transform after:ease-out after:-z-10 hover:after:translate-x-0 hover:after:translate-y-0 active:bg-red-400 hover:outline-none "
         >
-          Next
+          Next👉🏻
         </button>
       </Link>
     </>
