059909465307b6af07f9c7b5e2e3bfcf8d273b3b
leejaelll <jayaom91076@gmail.com>
2023-05-03 06:03:20 +0900

    style: GameResultPage 컴포넌트 스타일링 완성

--- src/pages/GameResultPage.tsx
+++ src/pages/GameResultPage.tsx
@@ -1,5 +1,6 @@
 import { useSelector } from 'react-redux';
 import PlayerResult from '../components/PlayerResult';
+import TeamScore from '../components/TeamScore';
 
 export default function GameResultPage() {
   const state = useSelector((state) => state);
@@ -16,8 +17,6 @@ export default function GameResultPage() {
     (player) => player.teamName === teamArray[1],
   );
 
-  console.log(homeTeam, awayTeam);
-
   // homeTeam의 총 점수 & awayTeam의 총 점수
   const TotalScoreOfHome = homeTeam
     .map((el) => el.score)
@@ -29,49 +28,55 @@ export default function GameResultPage() {
 
   return (
     <>
-      <h1>게임 결과 페이지</h1>
-      <div>
-        {TotalScoreOfHome} vs {TotalScoreOfAway}
+      <div className="flex justify-center items-center gap-20 mt-20 mb-10">
+        <TeamScore teamName={teamArray[0]} teamScore={TotalScoreOfHome} />
+        <div className="text-4xl">VS</div>
+        <TeamScore teamName={teamArray[1]} teamScore={TotalScoreOfAway} />
       </div>
-      <div>
-        <h1>{teamArray[0]}</h1>
-        <table className=" border-separate border-spacing-2 border border-slate-400">
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Score</th>
-              <th>Rebound</th>
-              <th>Assist</th>
-              <th>Steal</th>
-              <th>Foul</th>
-            </tr>
-          </thead>
-          <tbody>
-            {homeTeam.map((element) => (
-              <PlayerResult key={element.id} player={element} />
-            ))}
-          </tbody>
-        </table>
-      </div>
-      <div>
-        <h1>{teamArray[1]}</h1>
-        <table className=" border-separate border-spacing-2 border border-slate-400">
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Score</th>
-              <th>Rebound</th>
-              <th>Assist</th>
-              <th>Steal</th>
-              <th>Foul</th>
-            </tr>
-          </thead>
-          <tbody>
-            {awayTeam.map((element) => (
-              <PlayerResult key={element.id} player={element} />
-            ))}
-          </tbody>
-        </table>
+      <h2 className="mt-24 font-semibold text-2xl bg-slate-100 px-3 mb-3">
+        선수별 기록📝
+      </h2>
+      <div className="flex ">
+        <div className="w-1/2 flex flex-col text-center p-3">
+          <h1 className="text-lg font-light">{teamArray[0]}</h1>
+          <table className=" border-separate border-spacing-2 border border-slate-400">
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Score</th>
+                <th>Rebound</th>
+                <th>Assist</th>
+                <th>Steal</th>
+                <th>Foul</th>
+              </tr>
+            </thead>
+            <tbody>
+              {homeTeam.map((element) => (
+                <PlayerResult key={element.id} player={element} />
+              ))}
+            </tbody>
+          </table>
+        </div>
+        <div className="w-1/2 flex flex-col text-center p-3">
+          <h1 className="text-lg font-light">{teamArray[1]}</h1>
+          <table className=" border-separate border-spacing-2 border border-slate-400">
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Score</th>
+                <th>Rebound</th>
+                <th>Assist</th>
+                <th>Steal</th>
+                <th>Foul</th>
+              </tr>
+            </thead>
+            <tbody>
+              {awayTeam.map((element) => (
+                <PlayerResult key={element.id} player={element} />
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
     </>
   );
